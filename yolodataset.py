5aaca88b0d74c8d5fc606dbff3f9d40d13de4078
theovesy <theovesy@hotmail.fr>
2021-10-04 16:51:25 +0200

    fixing

--- yolodataset.py
+++ yolodataset.py
@@ -15,13 +15,13 @@ def get_args(i):
 # Pour generer les lignes a ajouter dans les fichiers texte de chaque image
 def yolo_line(row):
     line = "0 "
-    line += str((int(row[3])-int(row[1]))/2)
+    line += str(((int(row[3])-int(row[1]))/2)/img_size[0])
     line += " "
-    line += str((int(row[4])-int(row[2]))/2)
+    line += str(((int(row[4])-int(row[2]))/2)/img_size[1])
     line += " "
     line += str((int(row[3])-int(row[1]))/(img_size[0]))
     line += " "
-    line += str((int(row[4])-int(row[2]))/(img_size[0]))
+    line += str((int(row[4])-int(row[2]))/(img_size[1]))
     return line
 
 # Fonction principale
@@ -32,7 +32,7 @@ def write_files():
 
     # Pour chaque ligne du csv on ajoute dans le fichier lié a l'image correspondante
     for row in source_rows:
-        with open("labels/" + row[0][:-4], 'a') as writer:
+        with open("labels/" + row[0][:-4] + ".txt", 'a') as writer:
             writer.write(yolo_line(row) + "\n")
 
     source.close()
